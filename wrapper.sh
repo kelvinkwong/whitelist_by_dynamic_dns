ecf7df8c4376982d8dd4a17f79c40dac6a62a5b0
Kelvin Wong <kelvinwong@outlook.com.au>
2021-07-13 21:09:30 +1000

    Filter comments for the wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -2,5 +2,13 @@
 
 while read -r label fqdn port
 do
-    ./whitelist_by_dynamic_dns.sh $label $fqdn $port | tee -a /var/log/whitelist_by_dynamic_dns.log
+    if [[ ! -z $label ]]; then 
+    if [[ ! -z $fqdn ]]; then
+    if [[ ! -z $port ]]; then 
+    if [[ $label != "^#"* ]]; then 
+        ./whitelist_by_dynamic_dns.sh $label $fqdn $port | tee -a /var/log/whitelist_by_dynamic_dns.log
+    fi
+    fi 
+    fi
+    fi
 done < $1
