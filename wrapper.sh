f6d2894886e8d0a518d5616b980f59dd067a67f5
kelvin wong <kelvinwong@outlook.com.au>
2021-07-13 21:26:33 +1000

    add absolute path

--- wrapper.sh
+++ wrapper.sh
@@ -1,12 +1,15 @@
 #!/bin/bash 
 
+SCRIPT="$(realpath $0)"
+SCRIPTDIR="$(dirname $SCRIPT)"
+
 while read -r label fqdn port
 do
     if [[ ! -z $label ]]; then 
     if [[ ! -z $fqdn ]]; then
     if [[ ! -z $port ]]; then 
     if [[ $label != "^#"* ]]; then 
-        ./$1 $label $fqdn $port | tee -a /var/log/$1.log
+        $SCRIPTDIR/$1 $label $fqdn $port | tee -a /var/log/$1.log
     fi
     fi 
     fi
